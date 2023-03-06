e6e2d1a2df004944fdd62fc893a672a3f6ae58cb
Anderson Sousa <and3rsonsousa@outlook.com>
2023-03-06 18:26:53 -0300

    tag

--- src/root.tsx
+++ src/root.tsx
@@ -16,7 +16,6 @@ export default component$(() => {
 				<link rel="manifest" href="/manifest.json" />
 
 				<script
-					type="text/partytown"
 					dangerouslySetInnerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
