6d69a531f7ea538a49327bddbe93e5dcfad304cf
Anderson Sousa <and3rsonsousa@outlook.com>
2023-03-06 18:24:29 -0300

    no partytown

--- src/root.tsx
+++ src/root.tsx
@@ -4,7 +4,6 @@ import {
 	RouterOutlet,
 	ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
-import { QwikPartytown } from "./components/partytown/partytown";
 import { RouterHead } from "./components/router-head";
 
 import "./global.css";
@@ -15,21 +14,15 @@ export default component$(() => {
 			<head>
 				<meta charSet="utf-8" />
 				<link rel="manifest" href="/manifest.json" />
-				<QwikPartytown forward={["dataLayer.push"]} />
-				<script
-					async
-					type="text/partytown"
-					src="https://www.googletagmanager.com/gtag/js?id=GTM-544M7HL"
-				/>
 
-				{/* <script
+				<script
 					type="text/partytown"
 					dangerouslySetInnerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-544M7HL');"
-				/> */}
+				/>
 
 				<RouterHead />
 			</head>
