55f28e7ce1ac02d87ebf8b2382e87d57e20d56e5
Anderson Sousa <and3rsonsousa@outlook.com>
2023-03-07 12:44:19 -0300

    change

--- src/root.tsx
+++ src/root.tsx
@@ -25,14 +25,6 @@ export default component$(() => {
 				<meta charSet="utf-8" />
 				<link rel="manifest" href="/manifest.json" />
 
-				<script
-					dangerouslySetInnerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-544M7HL');"
-				/>
-
 				<RouterHead />
 			</head>
 			<body lang="pt-br">
@@ -46,6 +38,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				</noscript>
 				<RouterOutlet />
 				<ServiceWorkerRegister />
+				<script
+					dangerouslySetInnerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-544M7HL');"
+				/>
 			</body>
 		</QwikCityProvider>
 	);
