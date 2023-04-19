ebc2c159b4b23ac3a0596ce7c17c48c759790066
Anderson Sousa <and3rsonsousa@outlook.com>
2023-04-19 11:03:33 -0300

    separator

--- src/components/separator.tsx
+++ src/components/separator.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
-export default component$(({ class }: { class?: string }) => (
+export default component$(({ className }: { className?: string }) => (
 	<div
-		class={`max-w-md mx-auto w-1/2 bg-gradient-to-r from-transparent to-transparent h-0.5 ${class}`}
+		class={`max-w-md mx-auto w-1/2 bg-gradient-to-r from-transparent to-transparent h-0.5 ${className}`}
 	/>
 ));
